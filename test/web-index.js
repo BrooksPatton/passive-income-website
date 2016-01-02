ad7055fcc040246ad218028ee6bb67fc12c2e04e
BrooksPatton <brooks-patton@sbcglobal.net>
2016-01-02 14:52:15 -0700

    added a hello world to the / route

---
+++ test/web-index.js
@@ -0,0 +1,23 @@
+var chai = require('chai');
+var supertest = require('supertest');
+
+var app = require('../app');
+
+var should = chai.should();
+
+describe('Sending a GET to /', function() {
+	describe('should succeed', function() {
+		it('in getting some text', function(done) {
+			supertest(app)
+				.get('/')
+				.expect(200)
+				.end(function(err, res){
+					if(err) return done(err);
+
+					res.text.should.be.equal('hello world');
+
+					done();
+				});
+		});
+	});
+});
